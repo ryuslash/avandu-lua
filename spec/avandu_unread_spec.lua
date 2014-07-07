5a330c8e0aeb396cdcc8a93c0e273781c5b78135
Tom Willemse <tom@ryuslash.org>
2014-07-07 23:47:05 +0200

    Minor cleanup of some tests

--- spec/avandu_unread_spec.lua
+++ spec/avandu_unread_spec.lua
@@ -34,99 +34,81 @@ describe('Getting unread articles from Tiny Tiny RSS', function ()
                      'The file ' .. credfile .. ' has incorrect permissions')
   end)
 
-  it('should return an error if no url was set', function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
+  describe("with proper files", function ()
+    before_each(function ()
+      posix.access = yes
+      posix.stat = ureadable
+      io.open = fakefile
+      avandu.ttrss_url = url
+    end)
 
-    avandu.ttrss_url = nil
+    it('should return an error without a URL', function ()
+      avandu.ttrss_url = nil
+      local result, err = avandu.unread()
 
-    local result, err = avandu.unread()
+      assert.falsy(result)
+      assert.are.same({message = 'no URL set'}, err)
+      assert.are.same(avandu.Exception, getmetatable(err))
+    end)
 
-    assert.falsy(result)
-    assert.are.same({message = 'no URL set'}, err)
-    assert.are.same(avandu.Exception, getmetatable(err))
-  end)
+    it("should send the proper parameters to the request", function ()
+      https.request = function (params)
+         assert.are.same(params.url, url)
+         assert.are.same(params.method, 'POST')
+         assert.are.same(
+            {['Content-Length'] = params.source():len()}, params.headers)
+         assert.are.same(params.protocol, 'tlsv1')
+         assert.truthy(params.sink)
+         assert.truthy(params.source)
+      end
 
-  it("should send the proper parameters to the request", function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
+      avandu.unread()
+    end)
 
-    https.request = function (params)
-       assert.are.same(params.url, url)
-       assert.are.same(params.method, 'POST')
-       assert.are.same(
-          {['Content-Length'] = params.source():len()}, params.headers)
-       assert.are.same(params.protocol, 'tlsv1')
-       assert.truthy(params.sink)
-       assert.truthy(params.source)
-    end
+    it("should return a proper number of unread items", function ()
+      https.request = function (params)
+         params.sink('{"status": 0, "content": {"unread": 15}}')
+         return '', 200, {}, ''
+      end
 
-    avandu.ttrss_url = url
-    avandu.unread()
-  end)
+      local result, err = avandu.unread()
+      assert.are.same(15, result)
+    end)
 
-  it("should return a proper number of unread items", function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
+    it("should return an exception upon a 404 HTTP error", function ()
+      https.request = function (params)
+         return '', 404, {}, 'Not Found'
+      end
 
-    https.request = function (params)
-       params.sink('{"status": 0, "content": {"unread": 15}}')
-       return '', 200, {}, ''
-    end
+      local results, err = avandu.unread()
+      assert.falsy(results)
+      assert.are.same({message = 'URL not found', url = url, code = 404,
+                       status = 'Not Found'}, err)
+      assert.are.same(avandu.Exception, getmetatable(err))
+    end)
 
-    avandu.ttrss_url = url
-    local result, err = avandu.unread()
-    assert.are.same(15, result)
-  end)
+    it("should return an exception upon any unexpected HTTP status", function ()
+      https.request = function (params)
+         return '', 500, {}, 'Internal Server Error'
+      end
 
-  it("should return an exception upon a 404 HTTP error", function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
+      local results, err = avandu.unread()
+      assert.falsy(results)
+      assert.are.same({message = 'Unexpected HTTP status returned',
+                       url = url, code = 500,
+                       status = 'Internal Server Error'}, err)
+      assert.are.same(avandu.Exception, getmetatable(err))
+    end)
 
-    https.request = function (params)
-       return '', 404, {}, 'Not Found'
-    end
+    it("should return -1 for a non-0 status", function ()
+      https.request = function (params)
+         params.sink('{"status": 1}')
+         return '', 200, {}, ''
+      end
 
-    local results, err = avandu.unread()
-    assert.falsy(results)
-    assert.are.same({message = 'URL not found', url = url, code = 404,
-                     status = 'Not Found'}, err)
-    assert.are.same(avandu.Exception, getmetatable(err))
-  end)
-
-  it("should return an exception upon any unexpected HTTP status", function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
-
-    https.request = function (params)
-       return '', 500, {}, 'Internal Server Error'
-    end
-
-    local results, err = avandu.unread()
-    assert.falsy(results)
-    assert.are.same({message = 'Unexpected HTTP status returned',
-                     url = url, code = 500,
-                     status = 'Internal Server Error'}, err)
-    assert.are.same(avandu.Exception, getmetatable(err))
-  end)
-
-  it("should return -1 for a non-0 status", function ()
-    posix.access = yes
-    posix.stat = ureadable
-    io.open = fakefile
-
-    https.request = function (params)
-       params.sink('{"status": 1}')
-       return '', 200, {}, ''
-    end
-
-    local results, err = avandu.unread()
-    assert.are.same(-1, results)
-    assert.falsy(err)
+      local results, err = avandu.unread()
+      assert.are.same(-1, results)
+      assert.falsy(err)
+    end)
   end)
 end)
